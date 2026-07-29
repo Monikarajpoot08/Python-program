19e0bc76fea81da80d32338fe9d9b6cb0ec76528
monika rajpoot <rajpootmonika08@gmail.com>
2026-07-29 11:54:49 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+# Dictionary program
+# Dictionary is a built-in DT in python that stores key-value pair
+info={"hh":11,"pp":99}
+print(info["pp"]) #to access the value in dictionary , key is used
+print(info) #prints dictionary
+info["yy"]=55 # adds a new key value pair
+print(info)
+info.update({"hh":22}) #updates the key-value pair
+print(info)
