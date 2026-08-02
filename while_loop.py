3f86d3b51a54ddede81e106ba00ed332d4dd01a4
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-02 10:31:36 +0530

    while loop

---
+++ while_loop.py
@@ -0,0 +1,8 @@
+# while loop
+list=[1,2,3,4,5]
+while len(list):
+    print("hey monika!! ")
+    list.pop()
+    #deletes the no. from the list after every iteration
+    #and when the list becomes empty the loop will stop otherwise it will be infinite loop
+    print(list)
