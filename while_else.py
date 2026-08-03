6c763ad73865c891fcfb57fa2d3241321933c335
monika rajpoot <rajpootmonika08@gmail.com>
2026-08-03 17:00:43 +0530

    while-else loop

---
+++ while_else.py
@@ -0,0 +1,11 @@
+# while-else loop (built-in feature in python only)
+count=1
+while count<1:
+    print(count)
+    count+=1   #here execution is successfully  done despite the condition is false
+    #if break is used in bw the loop then it doesn't execute else block
+else:
+    print("bye")
+
+
+
